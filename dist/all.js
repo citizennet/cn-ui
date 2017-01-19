641e71e13234bce347ba656b45ff2cd93ad4c555
Lee Leathers <leeleathers@gmail.com>
2017-01-19 14:26:03 -0800

    chore(*): build

--- dist/all.js
+++ dist/all.js
@@ -832,7 +832,7 @@
 
           if (paging.skip !== 1) {
             pages.push({
-              label: '←',
+              label: '\u2190',
               skip: paging.skip - 1
             });
           }
@@ -867,7 +867,7 @@
 
           if (paging.skip !== paging.last) {
             pages.push({
-              label: '→',
+              label: '\u2192',
               skip: paging.skip + 1
             });
           }
@@ -1031,23 +1031,23 @@
             $timeout(activate, 500);
             //$timeout(activate, 800); // twice for good measure
           } else {
-              var bottomOffset = attrs.cnResponsiveHeight || 0;
-              var height = w.height() - topOffset - bottomOffset;
-              var overflow = attrs.cnResponsiveOverflow || 'auto';
-              height = height ? height + 'px' : 'auto';
-              //console.log('attrs.cnSetMaxHeight:', attrs.cnSetMaxHeight);
-              if (_.has(attrs, 'cnSetMaxHeight')) {
-                elem.css({
-                  'max-height': height,
-                  'overflow': overflow
-                });
-              } else {
-                elem.css({
-                  'height': height,
-                  'overflow': overflow
-                });
-              }
+            var bottomOffset = attrs.cnResponsiveHeight || 0;
+            var height = w.height() - topOffset - bottomOffset;
+            var overflow = attrs.cnResponsiveOverflow || 'auto';
+            height = height ? height + 'px' : 'auto';
+            //console.log('attrs.cnSetMaxHeight:', attrs.cnSetMaxHeight);
+            if (_.has(attrs, 'cnSetMaxHeight')) {
+              elem.css({
+                'max-height': height,
+                'overflow': overflow
+              });
+            } else {
+              elem.css({
+                'height': height,
+                'overflow': overflow
+              });
             }
+          }
         } else {
           elem.css({ 'height': '' });
         }
@@ -1389,7 +1389,7 @@ var _extends = Object.assign || function (target) { for (var i = 1; i < argument
       },
       link: function link($scope, elem) {
         var ogText = $scope.text || '',
-            shortText = ogText.length > $scope.size ? ogText.substr(0, $scope.size) + '…' : ogText,
+            shortText = ogText.length > $scope.size ? ogText.substr(0, $scope.size) + '\u2026' : ogText,
             truncated = false;
 
         function truncate() {
