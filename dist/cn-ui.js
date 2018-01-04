140dc6e1213cceef2abeec177ae3c1b233a68cbb
Chris Cornwell <chris@citizennet.com>
2018-01-04 11:01:53 -0800

    fix: only update preview path on model value change in media upload component if the new value is a path

--- dist/cn-ui.js
+++ dist/cn-ui.js
@@ -1028,7 +1028,7 @@
     var vm = this;
 
     vm.uploadFile = uploadFile;
-    $scope.$watch('vm.ngModel', activate);
+    $scope.$watch('vm.ngModel', updatePreview);
 
     activate();
 
@@ -1039,6 +1039,12 @@
         vm.filePath = $sce.trustAsResourceUrl(vm.ngModel);
       } else if (vm.cnFileType === 'video' && vm.ngModel) {
         vm.filePath = $sce.trustAsResourceUrl(vm.ngModel.media);
+      }
+    }
+
+    function updatePreview() {
+      if (vm.cnFileType === 'image' && vm.ngModel && vm.ngModel.includes("/")) {
+        vm.filePath = $sce.trustAsResourceUrl(vm.ngModel);
       }
     }
 
