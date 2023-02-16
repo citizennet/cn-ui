496f12a1f94491a23259fe306ce527e9e1606276
Tara Strauss <tara.m.strauss@gmail.com>
2023-02-16 10:41:07 -0800

    add logs for debugging

--- src/cn-media-upload.directive.js
+++ src/cn-media-upload.directive.js
@@ -160,13 +160,17 @@
     }
 
     function setFilePath(response) {
+      console.log("setFilePath");
+      console.log("response: ", response);
       cfpLoadingBar.complete();
       if (vm.cnImagePreviews) {
         delete vm.cnImagePreviews[vm.cnKey];
       }
       vm.cnModelValueKey = vm.cnModelValueKey || vm.cnForm.valueProperty;
       vm.ngModel = response[vm.cnModelValueKey || 'media_id_string'];
+      console.log("vm.cnPreviewPath: ", vm.cnPreviewPath);
       vm.filePath = $sce.trustAsResourceUrl(response[vm.cnPreviewPath || 'cn_preview_url']);
+      console.log("vm.filePath: ", vm.filePath);
       
       $scope.$emit("cnMediaUpload.uploaded", {cn_preview_url: response[vm.cnPreviewPath || 'cn_preview_url'], media_key: vm.ngModel});
 
